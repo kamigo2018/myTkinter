692ae3c7384ab9f8cd3386f2b03344b01e63b7cc
kamigo2018 <kamigo2014@126.com>
2020-01-14 17:03:21 +0800

    20200114：Step1.修改窗体title和icon。

--- HelloTkinter.py
+++ HelloTkinter.py
@@ -1,26 +1,34 @@
 import tkinter as tk
 
+from PIL import ImageTk,Image
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-root.geometry("600x400") # 百度经验，设置窗体大小
-app.mainloop()
+        self.create()
+    
+    def create(self):
+        # 定义Master的标题栏
+        self.master.title("GUI应用-Tk")
+        # 替换应用的图标
+        self.master.iconbitmap("sources/blackEight.ico")
+        # 设置窗体最小大小，和最大大小：
+        self.master.maxsize(1200,800)
+        self.master.minsize(300,200)
+        
+        
+        # 创建菜单栏
+        
+        # 创建显示文字的text widget 的Frame
+        
+        # 创建输入文字的Frame
+        
+#================================================
+#==================== MAIN ======================
+#================================================
+if __name__=="__main__":
+    root = tk.Tk()                 # 只能有一个tk.Tk()产生一个。
+    app = Application(master=root)
+    app.mainloop()
